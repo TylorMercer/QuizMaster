ca98b474f145e26505152fd4a5f6647750fffab3
HelexBlade <tylor.mercer@hotmail.com>
2022-06-29 10:54:40 -0230

    Adding test questions
    Testing questions for scriptable object.

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -7,4 +7,18 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]
     [SerializeField] string question = "Enter new Question text here";
+    [SerializeField] string[] answers = new string[4];
+    [SerializeField] int correctAnswerIndex;
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public string GetAnswer(int index){
+        return answers[index];
+    }
+    
+    public int GetCorrectAnswerIndex(){
+        return correctAnswerIndex;
+    }
 }
